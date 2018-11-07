ef669dbe031f9e267ff8f9bf7c74e665e5bdd9cf
Copperly <aidancspendlove@gmail.com>
2018-11-07 15:20:33 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "using apt to try to install youtube-dl (dependency, if it is already installed you don't need to worry)"
-sudo apt install youtube-dl
+sudo apt install youtube-dl ffmpeg
 sudo cp youtube /usr/local/bin
 echo "fully installed"
 exit
