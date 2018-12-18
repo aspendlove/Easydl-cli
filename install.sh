f81323ecd1b48f77aed77ec3ba8b5574ca180983
Copperly <aidancspendlove@gmail.com>
2018-12-18 11:42:36 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 echo "using pip to try to install youtube-dl and apt to install ffmpeg (if pip is not installed apt will try to install it) youtube-dl is a dependency, if it is already installed you don't need to worry"
 sudo apt install python-pip ffmpeg
 sudo pip install --upgrade youtube_dl
-sudo cp youtube /usr/local/bin
+sudo cp easydl /usr/local/bin
 echo "fully installed, you can delete this folder if you want"
 exit
