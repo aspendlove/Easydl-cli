88efd6dd1ac4d2f74723beb09c67cbd7623a188d
Copperly <aidancspendlove@gmail.com>
2019-03-14 09:42:13 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
-echo "Do you want to install youtube-dl and ffmpeg via apt?"
+echo "Do you want to install python3-pip (to install youtube-dl) via apt?"
 read -p 'yes or no> ' yorn
 if [ "${yorn}" = 'yes' ]
 then
   echo '
   '
-  echo 'installing youtube-dl and ffmpeg'
+  echo 'installing python3-pip'
   echo '
   '
-  sudo apt install youtube-dl ffmpeg
-  echo '
-  '
-  echo 'installing easydl'
-  echo '
-  '
-  
+  sudo apt install python3-pip
 else
   echo '
   '
-  echo 'alright, installing easydl'
+  echo 'alright'
+fi
+echo 'Do you want to install youtube-dl via pip3?'
+read -p 'yes or no> ' yorn
+if [ "${yorn}" = 'yes' ]
+then
+  echo 'alright, installing youtbe-dl'
+  sudo pip3 install youtube-dl
+  echo '
+  '
+  echo 'installing easydl'
+else
+  echo '
+  '
+  echo 'installing easydl'
 fi
 sudo cp "easydl" /usr/local/bin
+mkdir ~/.config/easydl/
+cp aliases.sh ~/.config/easydl/
+cp youtube_commands.sh ~/.config/easydl/
+
 echo '
 '
 echo "fully installed, you can delete this folder if you want"
