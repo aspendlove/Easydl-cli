5d1a8b59207012ff5c43fd47bfdce6dcd768f388
Copperly <aidancspendlove@gmail.com>
2019-04-12 11:34:15 -0600

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
+#variable to check if you need to restart
+restart=0
+#check if /usr/local/bin exists
+if [ ! -d "/usr/local/bin/" ]
+then
+	#if not create it
+	sudo mkdir /usr/local/
+	sudo mkdir /usr/local/bin/
+	#add it to path
+	export PATH=$PATH:/usr/local/bin
+	#and set the restart flag
+	restart=1
+fi
+#give all executables executable permissions
 chmod a+x aliases.sh easydl uninstall.sh youtube_commands.sh
-echo "Do you want to install python3-pip (to install youtube-dl) via apt?"
+#ask if you want to install python3-pip via apt
+echo "Do you want to install python3-pip (aka, pip3) via apt? This will be used to install youtube-dl later. If you don't want to install it here you can install it yourself separately"
 read -p 'yes or no> ' yorn
 if [ "${yorn}" = 'yes' ]
 then
+	#if yes, then install it via apt
   echo '
   '
   echo 'installing python3-pip'
@@ -11,31 +27,42 @@ then
   '
   sudo apt install python3-pip
 else
+	#if no then just say "alright"
   echo '
   '
   echo 'alright'
 fi
+#ask the user if they want to instal youtube-dl via pip3
 echo 'Do you want to install youtube-dl via pip3?'
 read -p 'yes or no> ' yorn
 if [ "${yorn}" = 'yes' ]
 then
+	#if yes then install youtube-dl
   echo 'alright, installing youtbe-dl'
   sudo pip3 install youtube-dl
   echo '
   '
   echo 'installing easydl'
 else
+	#otherwise say "installing easydl"
   echo '
   '
   echo 'installing easydl'
 fi
+#copy the main executable to user/local/bin/
 sudo cp "easydl" /usr/local/bin
+#make the config directory easydl
 mkdir ~/.config/easydl/
+#copy the files aliases.sh and youtube_commands.sh to that folder
 cp aliases.sh ~/.config/easydl/
 cp youtube_commands.sh ~/.config/easydl/
-
-echo '
-'
-echo "fully installed, you can delete this folder if you want"
+#check if restart flag is set
+if [ restart != 1 ]
+then
+	#if not then tell the user that the program is installed
+	echo "fully installed"
+else
+	#if so then tell them that they will need to reboot"
+	echo "you will need to reboot to fully install"
+fi
 exit
-
--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+#ask if you want to uninstall youtube-dl
+echo 'Do you want to uninstall youtube-dl via pip3?'
+read -p 'yes or no> ' yorn
+if [ "${yorn}" = 'yes' ]
+then
+  echo 'alright, uninstalling youtbe-dl'
+  #uninstall youtube-dl
+  sudo pip3 uninstall youtube-dl
+else
+  echo 'alright'
+fi
+#ask if you want to uninstall python3-pip aka pip3
 echo "Do you want to uninstall python3-pip (what was used to install youtube-dl) via apt?"
 read -p 'yes or no> ' yorn
 if [ "${yorn}" = 'yes' ]
@@ -8,31 +20,24 @@ then
   echo 'uninstalling python3-pip'
   echo '
   '
+  #uninstall python3-pip via apt
   sudo apt purge python3-pip
-else
-  echo '
-  '
-  echo 'alright'
-fi
-echo 'Do you want to uninstall youtube-dl via pip3?'
-read -p 'yes or no> ' yorn
-if [ "${yorn}" = 'yes' ]
-then
-  echo 'alright, uninstalling youtbe-dl'
-  sudo pip3 uninstall youtube-dl
-  echo '
-  '
+  echo ''
+  #tell the user that easydl is being uninstalled
   echo 'uninstalling easydl'
 else
   echo '
   '
-  echo 'uninstalling easydl'
+  #tell the user that easydl is being unintalled
+  echo 'alright, uninstalling easydl'
 fi
+#delete the easydl executable
 sudo rm /usr/local/bin/easydl
+#delete the configuration folder
 rm -rf ~/.config/easydl/
 
 echo '
 '
+#tell the user it's done
 echo "fully uninstalled"
 exit
-
