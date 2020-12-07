bdffd3024b7025d5d54715851e2b3838341151f7
copperly <aidancspendlove@gmail.com>
2020-12-06 20:06:44 -0700

    Add support for youtube-dlc

--- youtube_commands.sh
+++ youtube_commands.sh
@@ -2,10 +2,28 @@
 youtubeVIDEO()
 {
 #this is the command that will run to download a video ($1 will be replaced with the url)
-youtube-dl -f mp4 -o '%(title)s.%(ext)s' $1
+if command -v 'youtube-dl' &> /dev/null
+then
+  #youtube-dl command
+  youtube-dl -f mp4 -o '%(title)s.%(ext)s' $1
+elif command -v 'youtube-dlc' &> /dev/null
+then
+  #yt-dlc command
+  youtube-dlc -f mp4 -o '%(title)s.%(ext)s' $1
+else
+  echo 'youtube-dl or yt-dlc is required for more support please reference the readme'
+fi
 }
 youtubeAUDIO()
 {
 #this is the command that will run to download audio ($1 will be replaced with the url)
-youtube-dl -x --audio-format mp3 -o '%(title)s.%(ext)s' $1
+if command -v 'youtube-dl' &> /dev/null
+then
+  youtube-dl -x --audio-format mp3 -o '%(title)s.%(ext)s' $1
+elif command -v 'youtube-dlc' &> /dev/null
+then
+  youtube-dlc -x --audio-format mp3 -o '%(title)s.%(ext)s' $1
+else
+  echo 'youtube-dl or yt-dlc is required for more support please reference the readme'
+fi
 }
